14765c7873bd209f01198cee17129bf7fb46f828
beyondwen <921790476@qq.com>
2018-11-19 00:05:50 +0800

    修复部分问题

--- toscrape_book/pipelines.py
+++ toscrape_book/pipelines.py
@@ -1,63 +1,63 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
+# # -*- coding: utf-8 -*-
 #
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# class BookPipeline(object):
-#     review_rating_map = {
-#         'One': 1,
-#         'Two': 2,
-#         'Three': 3,
-#         'Four': 4,
-#         'Five': 5,
-#     }
+# # Define your item pipelines here
+# #
+# # Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#
+#
+# # class BookPipeline(object):
+# #     review_rating_map = {
+# #         'One': 1,
+# #         'Two': 2,
+# #         'Three': 3,
+# #         'Four': 4,
+# #         'Five': 5,
+# #     }
+# #
+# #     def process_item(self, item, spider):
+# #         rating = item.get('review_rating')
+# #         if rating:
+# #             item['review_rating'] = self.review_rating_map[rating]
+# #             return item
+#
+# import MySQLdb
+#
+#
+# class MySQLPipeline:
+#     # def open_spider(self, spider):
+#     #     db = spider.settings.get('MYSQL_DB_NAME', 'scrapy_db')
+#     #     host = spider.settings.get('MYSQL_HOST', 'localhost')
+#     #     port = spider.settings.get('MYSQL_PORT', 3306)
+#     #     user = spider.settings.get('MYSQL_USER', 'root')
+#     #     passwd = spider.settings.get('MYSQL_PASSWORD', 'wenhao151')
+#     #     self.db_conn = MySQLdb.connect(host=host, port=port, db=db, user=user, passwd=passwd, charset='utf8')
+#     #     self.db_cur = self.db_conn.cursor()
+#     #
+#     # def close_spider(self, spider):
+#     #     self.db_conn.commit()
+#     #     self.db_conn.close()
+#     #
+#     # def process_item(self, item, spider):
+#     #     self.insert_db(item)
+#     #     return item
+#     #
+#     # def insert_db(self, item):
+#     #     values = (
+#     #         item['name'],
+#     #         item['url'],
+#     #         item['password'],
+#     #     )
+#     #     sql = 'INSERT INTO books(book_name,book_url,book_password) VALUES (%s,%s,%s)'
+#     #     self.db_cur.execute(sql, values)
+#
+#     def __init__(self):
+#         self.conn = MySQLdb.connect('localhost', 'root', 'wenhao151', 'scrapy_db', charset='utf8', use_unicode=True)
+#         self.cursor = self.conn.cursor()
 #
 #     def process_item(self, item, spider):
-#         rating = item.get('review_rating')
-#         if rating:
-#             item['review_rating'] = self.review_rating_map[rating]
-#             return item
-
-import MySQLdb
-
-
-class MySQLPipeline:
-    # def open_spider(self, spider):
-    #     db = spider.settings.get('MYSQL_DB_NAME', 'scrapy_db')
-    #     host = spider.settings.get('MYSQL_HOST', 'localhost')
-    #     port = spider.settings.get('MYSQL_PORT', 3306)
-    #     user = spider.settings.get('MYSQL_USER', 'root')
-    #     passwd = spider.settings.get('MYSQL_PASSWORD', 'wenhao151')
-    #     self.db_conn = MySQLdb.connect(host=host, port=port, db=db, user=user, passwd=passwd, charset='utf8')
-    #     self.db_cur = self.db_conn.cursor()
-    #
-    # def close_spider(self, spider):
-    #     self.db_conn.commit()
-    #     self.db_conn.close()
-    #
-    # def process_item(self, item, spider):
-    #     self.insert_db(item)
-    #     return item
-    #
-    # def insert_db(self, item):
-    #     values = (
-    #         item['name'],
-    #         item['url'],
-    #         item['password'],
-    #     )
-    #     sql = 'INSERT INTO books(book_name,book_url,book_password) VALUES (%s,%s,%s)'
-    #     self.db_cur.execute(sql, values)
-
-    def __init__(self):
-        self.conn = MySQLdb.connect('localhost', 'root', 'wenhao151', 'scrapy_db', charset='utf8', use_unicode=True)
-        self.cursor = self.conn.cursor()
-
-    def process_item(self, item, spider):
-        insert_sql = """
-            insert into books(book_name,book_url,book_password)VALUES(%s,%s,%s)
-        """
-        self.cursor.execute(insert_sql, (item['name'], item['url'], item['password']))
-        self.conn.commit()
+#         insert_sql = """
+#             insert into books(book_name,book_url,book_password)VALUES(%s,%s,%s)
+#         """
+#         self.cursor.execute(insert_sql, (item['name'], item['url'], item['password']))
+#         self.conn.commit()
--- toscrape_book/spiders/saveBook.py
+++ toscrape_book/spiders/saveBook.py
@@ -5,15 +5,16 @@ import scrapy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from scrapy.linkextractors import LinkExtractor
 
 
 class BooksSpider(scrapy.Spider):
     name = 'bookstest'
-    allowed_domains = ['192.168.1.114:8086']
-    start_urls = ['http://192.168.1.114:8086/list']
+    allowed_domains = ['192.168.2.150:8086']
+    start_urls = ['http://192.168.2.150:8086/list']
 
     def parse(self, response):
-        f = open('D:\\pythonwork\\toscrape_book\\jsonFile.json', 'r')
+        f = open('D:\\pythonwork\\fullstack\\toscrape_book\\jsonFile.json', 'r')
         cookiessu = f.read()
         cookiessu = json.loads(cookiessu)
         # 进行循环
@@ -27,9 +28,9 @@ class BooksSpider(scrapy.Spider):
                 # 书名
                 bookname = le.css('.bookName::text')
 
-                # browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome Beta\Application\chromedriver.exe')
+                browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome Beta\Application\chromedriver.exe')
                 # 打开浏览器
-                browser = webdriver.Chrome()
+                # browser = webdriver.Chrome()
                 # 抽取具体链接
                 url = url1[0].extract()
                 # 抽取具体密码
@@ -39,15 +40,15 @@ class BooksSpider(scrapy.Spider):
 
                 # 请求目标地址
                 browser.get(url)
-                time.sleep(2)
+                time.sleep(1)
                 # 浏览器最大化
                 browser.maximize_window()
-                time.sleep(2)
+                time.sleep(1)
                 if cookiessu != "":
                     for cssu in cookiessu:
                         print(cssu)
                         browser.add_cookie(cssu)
-                    time.sleep(2)
+                    time.sleep(1)
                 else:
                     # 你的百度去帐号，保存到你的网盘肯定需要你自己的帐号密码
                     user_name = 'beyond文豪'
@@ -85,23 +86,23 @@ class BooksSpider(scrapy.Spider):
                 submit_button = browser.find_element(By.CLASS_NAME, "text")
                 # 提交
                 submit_button.click()
-                time.sleep(3)
+                time.sleep(2)
                 # print(getCookie)
                 # 保存到网盘
                 browser.find_element_by_css_selector(".zbyDdwb").click()
                 browser.find_element(By.CLASS_NAME, "x-button-box").find_element_by_xpath('//*[@data-button-id="b1"]').click()
                 time.sleep(3)
                 # 选取保存位置
-                pdf_book = browser.find_element_by_xpath('//*[@node-path="/我的小书屋"]')
-                pdf_book.click()
-                time.sleep(3)
+                browser.find_element_by_xpath('//*[@node-path="/我的小书屋"]').click()
+                time.sleep(2)
                 browser.find_element(By.CLASS_NAME, "dialog-footer").find_element_by_xpath('//*[@data-button-id="b35"]').click()
                 time.sleep(3)
                 browser.close()
-                # try:
-                #
-                # except:
-                #     print('save faile :' + url)
+            le = LinkExtractor(restrict_css='#pageno')
+            links = le.extract_links(response)
+            if links:
+                next_url = links[0].url
+                yield scrapy.Request(next_url, callback=self.parse,dont_filter=True)
         except:
             print('save faile :' + bookname)
             fileObject = open('faile.txt', 'w')
