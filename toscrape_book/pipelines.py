3b0be89a68a8547eff90b56fe8b7bec57f6c176d
wenhao176 <921790476@qq.com>
2018-12-12 14:52:24 +0800

    增量处理

--- toscrape_book/pipelines.py
+++ toscrape_book/pipelines.py
@@ -25,43 +25,24 @@ import MySQLdb
 
 
 class MySQLPipeline:
-    # def open_spider(self, spider):
-    #     db = spider.settings.get('MYSQL_DB_NAME', 'scrapy_db')
-    #     host = spider.settings.get('MYSQL_HOST', 'localhost')
-    #     port = spider.settings.get('MYSQL_PORT', 3306)
-    #     user = spider.settings.get('MYSQL_USER', 'root')
-    #     passwd = spider.settings.get('MYSQL_PASSWORD', '123456')
-    #     self.db_conn = MySQLdb.connect(host=host, port=port, db=db, user=user, passwd=passwd, charset='utf8')
-    #     self.db_cur = self.db_conn.cursor()
-    #
-    # def close_spider(self, spider):
-    #     self.db_conn.commit()
-    #     self.db_conn.close()
-    #
-    # def process_item(self, item, spider):
-    #     self.insert_db(item)
-    #     return item
-    #
-    # def insert_db(self, item):
-    #     values = (
-    #         item['name'],
-    #         item['url'],
-    #         item['password'],
-    #         item['save_state'],
-    #         item['id']
-    #     )
-    #     update_sql = """
-    #          update book set name = %s,url=%s,password =%s,save_state=%s where id = %s
-    #      """
-    #     self.db_cur.execute(update_sql, values)
-
     def __init__(self):
         self.conn = MySQLdb.connect('127.0.0.1', 'root', 'wenhao151', 'scrapy_db', charset='utf8', use_unicode=True)
         self.cursor = self.conn.cursor()
 
     def process_item(self, item, spider):
-        update_sql = """
-            update book set name = %s,url=%s,password =%s,save_state=%s where id = %s
-        """
-        self.cursor.execute(update_sql, (item['name'], item['url'], item['password'],item['save_state'],item['id']))
-        self.conn.commit()
+        try:
+            select_sql = "select * from book where name = %s"
+            value = [item['name']]
+            self.cursor.execute(select_sql, value)
+            result = self.cursor.fetchone()
+            if result:
+                pass
+            else:
+                update_sql = """
+                             insert into  book (name,url,password,save_state,main_url) value (%s,%s,%s,%s,%s)
+                         """
+                self.cursor.execute(update_sql, (item['name'], item['url'], item['password'], item['save_state'],item['main_url']))
+                self.conn.commit()
+        except Exception as e:
+            print(e)
+
